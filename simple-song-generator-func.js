8f897a7704226fea8e3783ddbf12ae5702c37bec
marrimg <marrimg@gmail.com>
2020-07-11 14:58:45 -0700

    replace clip notes

--- simple-song-generator-func.js
+++ simple-song-generator-func.js
@@ -9,7 +9,8 @@ function Note(pitch, start, duration){
 function addChord(notes) {
   var path = "live_set tracks 0 clip_slots 0 clip";
   var obj = new LiveAPI(path);
-  obj.call("set_notes");
+  obj.call("select_all_notes");
+  obj.call("replace_selected_notes");
   obj.call("notes", notes.length); 
   notes.forEach(function (note) { 
     obj.call("note", note.pitch, note.start.toFixed(4), note.duration.toFixed(4), note.velocity, note.muted);
@@ -21,13 +22,11 @@ function setNotes() {
   var d1 = new Dict("midiOutputVals");
   var keys = d1.getkeys();
   var notes = [];
-  post(JSON.stringify(d1.getkeys()), 'D1NEWWW');
   if (keys) {
     keys.forEach(function (key, index) {
       const notesFromKey = d1.get(key);
       notesFromKey && notesFromKey.forEach(function (note) {
-        var position = index;
-        notes.push(new Note(note, position, 1));
+        notes.push(new Note(note, index, 1));
       });
     });
     addChord(notes);
