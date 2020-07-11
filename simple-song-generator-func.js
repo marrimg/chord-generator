13d257434ae135fcdf67dc3645bf5395af956eea
marrimg <marrimg@gmail.com>
2020-07-11 14:17:51 -0700

    delete unused code

--- simple-song-generator-func.js
+++ simple-song-generator-func.js
@@ -11,14 +11,9 @@ function addChord(notes) {
   var obj = new LiveAPI(path);
   obj.call("set_notes");
   obj.call("notes", notes.length); 
-  // notes.forEach(function (note) { 
-  //   // post(note.pitch, 'HI FROM JS');
-  //   obj.call("note", note.pitch, note.start.toFixed(4), note.duration.toFixed(4), note.velocity, note.muted);
-  // });
-  for (i = 0; i < notes.length; i++){
-    // post(notes[i].pitch, 'HI FROM JS');
-    obj.call("note", notes[i].pitch, notes[i].start.toFixed(4), notes[i].duration.toFixed(4), notes[i].velocity, notes[i].muted);
-  }
+  notes.forEach(function (note) { 
+    obj.call("note", note.pitch, note.start.toFixed(4), note.duration.toFixed(4), note.velocity, note.muted);
+  });
   obj.call("done");
 }
 
@@ -28,61 +23,13 @@ function setNotes() {
   var notes = [];
   post(JSON.stringify(d1.getkeys()), 'D1NEWWW');
   if (keys) {
-    // keys.forEach(function (key) {
-    //   const notesFromKey = d1.get(key);
-    //   notesFromKey && notesFromKey.forEach(function (note, index) {
-    //     var position = index + 1;
-    //     notes.push(new Note(note, position, 1));
-    //   });
-    // });
-    for (i = 0; i < keys.length; i++){
-      const notesFromKey = d1.get(keys[i]);
-      for (j = 0; j < notesFromKey.length; j++){
-        // const notef = new Note(notesFromKey[j], position, 1);
-        // post(JSON.stringify(notesFromKey[j]), "hi");
-        notes.push(new Note(notesFromKey[j], i, 1));
-      }
-      // notesFromKey && notesFromKey.forEach(function (note, index) {
-      //   var position = index + 1;
-      //   notes.push(new Note(note, position, 1));
-      // });
-    }
-    // post(JSON.stringify(notes));
+    keys.forEach(function (key, index) {
+      const notesFromKey = d1.get(key);
+      notesFromKey && notesFromKey.forEach(function (note) {
+        var position = index;
+        notes.push(new Note(note, position, 1));
+      });
+    });
     addChord(notes);
   }
-  // var path = "live_set tracks 0 clip_slots 0 clip";
-  // var obj = new LiveAPI(path);
-  // // obj.call("create_clip", 0, 1);
-  // obj.call("set_notes");
-  // obj.call("notes", 1);
-  // obj.call("note", 33, "1.0", "1.0", 100, 0);
-  // obj.call("done");
 }
-
-// var path = "live_set tracks 0 clip_slots 0 clip";
-// var obj = new LiveAPI(path);
-// obj.call("create_clip", 0, 1);
-// // post(note2);
-// obj.call("set_notes");
-// obj.call("notes", 1);
-// obj.call("note", 33, "1.0", "1.0", 100, 0);
-// // obj.call("note", note2, "1.0", "1.0", 100, 0);
-// // obj.call("note", note3, "1.0", "1.0", 100, 0);
-// obj.call("done");
-
-// function addChord(notes) {
-//     var path = "live_set tracks 0 clip_slots 0 clip";
-//     var obj = new LiveAPI(path);
-//     obj.call("set_notes");
-//     obj.call("notes", 3);
-//     for (i = 0; i < notes.length; i++){
-//         obj.call("note", notes[i], "1.0", "1.0", 100, 0);
-//     }
-//     // obj.call("note", notes, "1.0", "1.0", 100, 0);
-//     post(notes);
-//     // obj.call("note", "65", "1.0", "1.0", 100, 0);
-//     // obj.call("note", "60", "1.0", "1.0", 100, 0);
-//     // obj.call("note", "55", "1.0", "1.0", 100, 0);
-//     // obj.call("note", "50", "1.0", "1.0", 100, 0);
-//     obj.call("done");
-// }
--- simple-song-generator-simplified.js
+++ simple-song-generator-simplified.js
@@ -1,30 +1,10 @@
 const maxApi = require('max-api');
-const { Scale, Chord, Note } = require('tonal');
+const { Chord, Note } = require('tonal');
 const Key = require('tonal-key');
 
-// const generateScale = (scale) => Scale.notes(scale);
 const generateChord = (chordName) => Chord.notes(chordName);
 const addOctaves = (chord, octave) => chord.map(item => item + octave);
 const chordToMidi = (chord) => chord.map((item) => Note.midi(item));
-
-// let chordName;
-// let chordType;
-// let chordProgression;
-// let measures;
-
-const lettersToNumberMap = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4
-};
-
-// const activebgoncolorMap = {
-//   1: [0.992, 0.188, 0.188, 1.000],
-//   2: [0.992, 0.506, 0.188, 1.000],
-//   3: [1.000, 0.710, 0.196, 1.000],
-//   4: [0.996, 0.894, 0.000, 1.000]
-// }
 
 const chordMap = {
   maj: {
@@ -53,13 +33,7 @@ let appState = {
   midiOutput: null,
 }
 
-// CHORD BUTTONS
-
-// const getNumberOfChords = (prog) => {
-//   const progArr = prog.match(/[a-z]/g);
-//   const uniqueItems = progArr.filter((item, index) => progArr.indexOf(item) === index);
-//   return uniqueItems.length;
-// };
+// CHORD DROPDOWNS
 
 const getSelectedChords = () => {
   const chords = {};
@@ -70,56 +44,6 @@ const getSelectedChords = () => {
     });
   return chords;
 }
-
-// const clearChordState = () => {
-//   Object.keys(appState.chords).forEach((item) => {
-//     appState.chords[item] = null;
-//   });
-// }
-
-// const activateUnonButtons = (isActive) => {
-//   Object.keys(appState.chordButtons).forEach((item) => {
-//     if(!appState.chordButtons[item].on) {
-//       appState.chordButtons[item] = { ...appState.chordButtons[item], active: isActive }
-//     }
-//   });
-// }
-
-// const updatePositionInPattern = (chord, isOn) => {
-//   if (isOn) {
-//       const existingPositions = Object.values(appState.chordButtons).map(item => item.positionInPatt);
-//       const patternInNumbers = convertPatternToNumbers(appState.pattern);
-//       const highestExistingVal = Math.max(...existingPositions);
-//       const highestValInPatt = Math.max(...patternInNumbers);
-//       if (highestExistingVal <= highestValInPatt) {
-//         const fillGaps = [...Array(highestExistingVal).keys()].map(item => item + 1);
-//         const gapToFill = fillGaps.find(item => !existingPositions.includes(item));
-//         if(gapToFill) {
-//           appState.chordButtons[chord].positionInPatt = gapToFill;
-//           appState.chordButtons[chord].activebgoncolor = activebgoncolorMap[gapToFill];
-//         } else if (!gapToFill && highestExistingVal < highestValInPatt) {
-//             appState.chordButtons[chord].positionInPatt = highestExistingVal + 1;
-//             appState.chordButtons[chord].activebgoncolor = activebgoncolorMap[highestExistingVal + 1];
-//         }
-//       }
-//   } else {
-//     appState.chordButtons[chord].positionInPatt = 0;
-//     appState.chordButtons[chord].activebgoncolor = null;
-//   }
-// }
-
-// const updateActiveChords = () => {
-//   const numberOfChords = appState.pattern ? getNumberOfChords(appState.pattern) : 0;
-//   const onItems = Object.values(appState.chordButtons).filter((item) => {
-//     return item.on;
-//   }).length;
-//   const freeSpaces = numberOfChords - onItems;
-//   if (freeSpaces === 0) {
-//     activateUnonButtons(false);
-//   } else if (freeSpaces > 0) {
-//     activateUnonButtons(true);
-//   }
-// }
 
 const updateChordState = (chord, value) => {
   appState.chords[chord] = value;
@@ -147,13 +71,8 @@ maxApi.addHandler('set4', (value) => {
 
 // PATTERN
 
-// const convertPatternToNumbers = (pattern) => {
-//   return pattern.match(/[a-z]/g).map(item => lettersToNumberMap[item]);
-// }
-
 maxApi.addHandler('updatePattern', (updatedPatt) => {
   appState.pattern = updatedPatt;
-  // clearChordState();
 });
 
 // MEASURES
@@ -171,18 +90,10 @@ maxApi.addHandler('updateKey', (root, scale) => {
 
 // CHORD OUTPUT
 
-// const placeChordsInPattern = (chords) => {
-//   const letters = appState.pattern.match(/[a-z]/g);
-//   letters.map((item) => {
-
-//   });
-// }
-
 const generateChordsForKey = (root, scale) => { 
   const chord = `${root} ${chordMap[scale].suffix}`
   const selChord = getSelectedChords();
   const chordPatt = Object.values(selChord);
-  // maxApi.post(chordPatt, 'SELCHORD');
   return Key.triads(chord, chordPatt);
 };
 
@@ -200,18 +111,7 @@ maxApi.addHandler('generateChords', () => {
     acc[index] = generateMidiChord(item);
     return acc;
   }, {});
-  // selectedChords.forEach((item) => {
-  //   chordsToWrite.push(generateMidiChord(item));
-  // });
   appState.midiOutput = chordsToWrite;
-  // maxApi.post(appState.midiOutput, 'SLE');
   maxApi.setDict('midiOutputVals', appState.midiOutput);
-  // maxApi.outlet(appState.midiOutput);
 });
 
-// ?
-
-// maxApi.addHandler('updateProgression', (button, value) => {
-//   // chordProgression = [a, b, c, d];
-//   maxApi.post(button, value, 'TEST');
-// });
