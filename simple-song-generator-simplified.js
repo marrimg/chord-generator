066c48bd4286e729ebd10a8fdd3109e7171940ae
marrimg <marrimg@gmail.com>
2020-07-13 21:28:25 -0700

    sub chords

--- simple-song-generator-simplified.js
+++ simple-song-generator-simplified.js
@@ -10,6 +10,10 @@ const generateChord = (chordName) => Chord.notes(chordName);
 const addOctaves = (chord, octave) => chord.map(item => item + octave);
 const chordToMidi = (chord) => chord.map((item) => Note.midi(item));
 const increaseOctave = (val) => val + OCTAVE_COMPENSATION;
+const getTriads = (chord, chordPatt) => Key.triads(chord, chordPatt);
+const getSubChords = (root, scale) => {
+  return `${root} ${chordMap[scale].suffix === 'major' ? 'minor' : 'major'}`;
+}
 
 const chordMap = {
   maj: {
@@ -35,6 +39,24 @@ let appState = {
     3: 'I',
     4: 'I'
   },
+  substitutions: {
+    1: false,
+    2: false,
+    3: false,
+    4: false
+  },
+  chordNames: {
+    1: 'C',
+    2: 'C',
+    3: 'C',
+    4: 'C'
+  },
+  chordSubNames: {
+    1: 'Cm',
+    2: 'Cm',
+    3: 'Cm',
+    4: 'Cm'
+  },
   octaves: {
     1: increaseOctave(DEFAULT_OCTAVE),
     2: increaseOctave(DEFAULT_OCTAVE),
@@ -42,6 +64,20 @@ let appState = {
     4: increaseOctave(DEFAULT_OCTAVE)
   },
   midiOutput: null,
+}
+
+// CHORD NAMES
+
+const setChordNames = (chords) => {
+  Object.keys(appState.chordNames).forEach((item, i) => {
+    appState.chordNames[item] = chords[i];
+  }); 
+}
+
+const setChordSubNames = (chords) => {
+  Object.keys(appState.chordSubNames).forEach((item, i) => {
+    appState.chordSubNames[item] = chords[i];
+  }); 
 }
 
 // CHORD DROPDOWNS
@@ -99,6 +135,30 @@ maxApi.addHandler('oct4', (value) => {
   appState.octaves[octave] = increaseOctave(value);
 });
 
+// SUBSTITUTIONS
+
+const getBooleanVal = (val) => val === 1 ? true : false;
+
+maxApi.addHandler('sub1', (value) => {
+  const chord = 1;
+  appState.substitutions[chord] = getBooleanVal(value);
+});
+
+maxApi.addHandler('sub2', (value) => {
+  const chord = 2;
+  appState.substitutions[chord] = getBooleanVal(value);
+});
+
+maxApi.addHandler('sub3', (value) => {
+  const chord = 3;
+  appState.substitutions[chord] = getBooleanVal(value);
+});
+
+maxApi.addHandler('sub4', (value) => {
+  const chord = 4;
+  appState.substitutions[chord] = getBooleanVal(value);
+});
+
 // PATTERN
 
 maxApi.addHandler('updatePattern', (updatedPatt) => {
@@ -128,27 +188,46 @@ const generateChordsForKey = (root, scale) => {
   const chord = `${root} ${chordMap[scale].suffix}`
   const selChord = getSelectedChords();
   const chordPatt = Object.values(selChord);
-  maxApi.post(Key.triads(chord, chordPatt));
-  return Key.triads(chord, chordPatt);
+  return getTriads(chord, chordPatt);
+};
+
+const generateSubChordsForKey = (root, scale) => { 
+  const chord = getSubChords(root, scale);
+  const selChord = getSelectedChords();
+  const chordPatt = Object.values(selChord);
+  return getTriads(chord, chordPatt);
 };
 
 const generateMidiChord = (chordName, oct) => {
   const chordNotes = generateChord(chordName);
   const chordWithOctaves = addOctaves(chordNotes, oct);
-  maxApi.post(chordWithOctaves, 'CHORDWIT');
   return chordToMidi(chordWithOctaves);
 }
 
-maxApi.addHandler('generateChords', () => {
+const setChordsAndSubChords = () => {
   const root = appState.key.root;
   const scale = appState.key.scale;
   const selectedChords = generateChordsForKey(root, scale);
-  const chordsToWrite = selectedChords.reduce((acc, item, index) => {
+  const subChords = generateSubChordsForKey(root, scale);
+  setChordNames(selectedChords);
+  setChordSubNames(subChords);
+}
+
+maxApi.addHandler('generateChords', () => {
+  setChordsAndSubChords();
+  const chordKeys = Object.keys(appState.chordNames);
+  const chordsWithSubs = chordKeys.map((item) => {
+    return !!appState.substitutions[item] ? appState.chordSubNames[item] : appState.chordNames[item];
+  });
+  maxApi.post(chordsWithSubs, 'CHORDSSUBS');
+  const chordsToWrite = chordsWithSubs.reduce((acc, item, index) => {
     const oct = appState.octaves[index + 1]
     acc[index] = generateMidiChord(item, oct);
     return acc;
   }, {});
+
   appState.midiOutput = chordsToWrite;
+  
   maxApi.setDict('midiOutputVals', appState.midiOutput);
   maxApi.outletBang();
 });
